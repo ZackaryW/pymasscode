654a4a1cfbda85b6c42230772cb5c913e1874f2f
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-04-06 12:55:44 -0700

    ref: reformat

--- pymasscode/__init__.py
+++
@@ -1,2 +0,0 @@
-from .loader import Loader
-from .dcls import Tag, Snippet, Folder
--- test.py
+++
@@ -1,8 +0,0 @@
-
-from pymasscode.dcls import Snippet
-
-
-w = Snippet.query(Snippet.q.name == "pypi upload", limit =1)
-script = w[0].content[0]["value"]
-exec(script)
-pass
